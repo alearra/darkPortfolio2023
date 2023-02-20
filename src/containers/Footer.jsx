cb880793aed772ba38ee96d1affa080014342dc8
alearra <alejandroarra.devv@gmail.com>
2023-02-20 17:55:13 -0300

    new style

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -5,14 +5,23 @@ export const Footer = () => {
   return (
     <footer>
       <div className="footer-item">
-        <a href="#top"><span>A</span></a>
+        <a href="#top">
+          <span>A</span>
+        </a>
         <p>Alejandro Arra</p>
       </div>
       <div className="footer-social">
-      <a href="mailto:alejandroarradev@gmail.com"><img src="https://img.icons8.com/ios/50/7D9286/filled-message.png"/></a>
-      <a href="https://www.linkedin.com/in/alejandroarra-projectmanager-dev/" target="_blank"><img src="https://img.icons8.com/ios/50/7D9286/linkedin.png"/></a>
-      <img src="https://img.icons8.com/ios/50/7D9286/instagram-new--v1.png"/>
-      <img src="https://img.icons8.com/ios/50/7D9286/twitter-squared.png"/>
+        <a href="mailto:alejandroarradev@gmail.com">
+          <img src="https://img.icons8.com/ios/50/ffffff/filled-message.png" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/alejandroarra-projectmanager-dev/"
+          target="_blank"
+        >
+          <img src="https://img.icons8.com/ios/50/ffffff/linkedin.png" />
+        </a>
+        <img src="https://img.icons8.com/ios/50/ffffff/instagram-new--v1.png" />
+        <img src="https://img.icons8.com/ios/50/ffffff/twitter-squared.png" />
       </div>
     </footer>
   );
